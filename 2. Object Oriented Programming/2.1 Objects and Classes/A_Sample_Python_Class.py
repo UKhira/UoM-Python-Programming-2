90a733cfbb5be7b188f00405916ce7a5bca2c9b3
Udith Kavishka <udithmanohara@gmail.com>
2023-05-21 14:37:21 +0530

    modify

--- 2. Object Oriented Programming/2.1 Objects and Classes/A_Sample_Python_Class.py
+++ 2. Object Oriented Programming/2.1 Objects and Classes/A_Sample_Python_Class.py
@@ -7,14 +7,31 @@ class Monster:
     
     #constructor
     def __init__(self, age, name):
-        #instance attributes
-        self.age = age
+        #instance attributes,  means each objects in monster class has different values for age, name attributes
+        self.age = age          # 'self' is a method that use to refer current object
         self.name = name
         
         #private attribute by convention
-        self._is_innocent = None
+        self._is_innocent = None            #private attributes are defined using a leading underscore "_"
         
     # instance method,  methods means behavior of an object
     def steal(self, warrior):
         warrior.lose_stick()
+        
+  
+      
+# creating an instance
+monster1 = Monster(15, "Ork")
+
+# accessing attributes
+print(monster1.age,"\n",monster1.name)
+
+# changing values
+monster1.age = 10
+print(monster1.age)
+
+# creating another instance
+monster2 = Monster(20, "Goblin")
+print(monster2.name)
+
         
